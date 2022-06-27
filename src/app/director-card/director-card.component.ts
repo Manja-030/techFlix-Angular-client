ce1ced532903df35bc2bbf877f49d385fe94a356
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-06-27 15:47:34 +0200

    update director-component, synopsis-component

--- src/app/director-card/director-card.component.ts
+++ src/app/director-card/director-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-director-card',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      directorName: string,
+      directorBio: string,
+      directorBirth: string}
+  ) { }
 
   ngOnInit(): void {
   }
 
 }
+
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
+import { SynopsisCardComponent } from '../synopsis-card/synopsis-card.component';
+import { GenreCardComponent } from './../genre-card/genre-card.component';
+import { DirectorCardComponent } from './../director-card/director-card.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,7 +14,7 @@ import { FetchApiDataService } from '../fetch-api-data.service'
 export class MovieCardComponent implements OnInit {
 
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(public fetchApiData: FetchApiDataService, public dialog: MatDialog, public snackbar: MatSnackBar) { }
 
 ngOnInit(): void {
   this.getMovies();
@@ -22,4 +27,20 @@ getMovies(): void {
       return this.movies;
     });
   }
+
+  getSynopsis(movieDescription: string): void {
+    this.dialog.open(SynopsisCardComponent, {
+      width: "500px", 
+      data: {movieDescription}
+     });
+    }
+    getDirector(directorName: string, directorBio: string, directorBirth: string): void {
+      this.dialog.open(DirectorCardComponent, {
+        data: { directorName, directorBio, directorBirth},
+        width: '500px',
+       });
+      }
+    
+     
+  
 }
