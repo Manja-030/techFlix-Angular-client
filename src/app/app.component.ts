179d14030cc6bab28bb82d144dc3d45bf773f1cc
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-06-29 09:07:18 +0200

    add style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 //import { MovieCardComponent } from './movie-card/movie-card.component';
 //import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
